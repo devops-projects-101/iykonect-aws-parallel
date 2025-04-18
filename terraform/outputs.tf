1ede2ea38554ce2bb21a5fabe6fae03e3de3c56d
kabilarajah <kabilarajahy@gmail.com>
2025-04-18 10:00:01 +0530

    Format code for consistency in EC2 module configuration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,27 +1,34 @@
 output "instance_public_ip" {
-  value = module.ec2.public_ip
+  description = "Public IP address of the EC2 instance"
+  value       = module.ec2.public_ip
 }
 
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
 }
 
 output "api_endpoint" {
-  value = module.ec2.api_endpoint
+  description = "HTTP endpoint for the API service"
+  value       = module.ec2.api_endpoint
 }
 
 output "prometheus_endpoint" {
-  value = module.ec2.prometheus_endpoint
+  description = "HTTP endpoint for Prometheus"
+  value       = module.ec2.prometheus_endpoint
 }
 
 output "grafana_endpoint" {
-  value = module.ec2.grafana_endpoint
+  description = "HTTP endpoint for Grafana"
+  value       = module.ec2.grafana_endpoint
 }
 
 output "sonarqube_endpoint" {
-  value = module.ec2.sonarqube_endpoint
+  description = "HTTP endpoint for SonarQube"
+  value       = module.ec2.sonarqube_endpoint
 }
 
 output "renderer_endpoint" {
-  value = module.ec2.renderer_endpoint
+  description = "HTTP endpoint for Grafana renderer"
+  value       = module.ec2.renderer_endpoint
 }
