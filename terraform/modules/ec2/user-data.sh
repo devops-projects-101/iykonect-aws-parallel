797d86914536709554675cc23e43852fd7ad8324
kabilarajah <kabilarajahy@gmail.com>
2025-04-18 09:43:29 +0530

    Set aws_region variable in user data script for AWS CLI configuration

--- terraform/modules/ec2/user-data.sh
+++ terraform/modules/ec2/user-data.sh
@@ -12,6 +12,7 @@ sudo systemctl enable docker
 echo "[$(date)] Installed required packages"
 
 # Configure AWS CLI
+aws_region="eu-west-1"
 aws configure set aws_access_key_id "${aws_access_key}"
 aws configure set aws_secret_access_key "${aws_secret_key}"
 aws configure set region "${aws_region}"
