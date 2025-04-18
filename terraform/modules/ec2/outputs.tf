5de3bdc193662cd0d46307d1391f43603efd988b
kabilarajah <kabilarajahy@gmail.com>
2025-04-19 00:13:53 +0530

    Refactor EC2 module outputs to use aws_spot_instance_request for instance ID and public IP

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -1,29 +1,29 @@
 output "instance_id" {
-  value = aws_instance.main.id
+  value = aws_spot_instance_request.main.spot_instance_id
 }
 
 output "public_ip" {
-  value = aws_instance.main.public_ip
+  value = aws_spot_instance_request.main.public_ip
 }
 
 output "api_endpoint" {
-  value = "http://${aws_instance.main.public_ip}:8000"
+  value = "http://${aws_spot_instance_request.main.public_ip}:8000"
 }
 
 output "prometheus_endpoint" {
-  value = "http://${aws_instance.main.public_ip}:9090"
+  value = "http://${aws_spot_instance_request.main.public_ip}:9090"
 }
 
 output "grafana_endpoint" {
-  value = "http://${aws_instance.main.public_ip}:3100"
+  value = "http://${aws_spot_instance_request.main.public_ip}:3100"
 }
 
 output "sonarqube_endpoint" {
-  value = "http://${aws_instance.main.public_ip}:9000"
+  value = "http://${aws_spot_instance_request.main.public_ip}:9000"
 }
 
 output "renderer_endpoint" {
-  value = "http://${aws_instance.main.public_ip}:8081"
+  value = "http://${aws_spot_instance_request.main.public_ip}:8081"
 }
 
 output "security_group_id" {
