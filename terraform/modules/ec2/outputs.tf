480f42689d2de0adbdb54a98c9939ef0f399af52
kabilarajah <kabilarajahy@gmail.com>
2025-04-18 09:56:16 +0530

    Add additional output endpoints for API, Prometheus, Grafana, SonarQube, and Renderer in EC2 module

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -5,3 +5,23 @@ output "instance_id" {
 output "public_ip" {
   value = aws_instance.main.public_ip
 }
+
+output "api_endpoint" {
+  value = "http://${aws_instance.main.public_ip}:8000"
+}
+
+output "prometheus_endpoint" {
+  value = "http://${aws_instance.main.public_ip}:9090"
+}
+
+output "grafana_endpoint" {
+  value = "http://${aws_instance.main.public_ip}:3100"
+}
+
+output "sonarqube_endpoint" {
+  value = "http://${aws_instance.main.public_ip}:9000"
+}
+
+output "renderer_endpoint" {
+  value = "http://${aws_instance.main.public_ip}:8081"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,23 @@ output "instance_public_ip" {
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "api_endpoint" {
+  value = module.ec2.api_endpoint
+}
+
+output "prometheus_endpoint" {
+  value = module.ec2.prometheus_endpoint
+}
+
+output "grafana_endpoint" {
+  value = module.ec2.grafana_endpoint
+}
+
+output "sonarqube_endpoint" {
+  value = module.ec2.sonarqube_endpoint
+}
+
+output "renderer_endpoint" {
+  value = module.ec2.renderer_endpoint
+}
